df596b1e92a944cc4cfa781f1f36e83d55273f83
janaka <janaka.weerarathna@ivory-cr.com>
2019-06-13 09:33:24 +0530

    added android mvvm 01

---
+++ app/src/main/java/com/ivory/mvvmdemoapp/view/MainActivity.java
@@ -0,0 +1,37 @@
+package com.ivory.mvvmdemoapp.view;
+
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.ivory.mvvmdemoapp.R;
+import com.ivory.mvvmdemoapp.databinding.ActivityMainBinding;
+//import com.ivory.mvvmdemoapp.databinding.ContentMainBinding;
+import com.ivory.mvvmdemoapp.viewmodel.LoginViewModel;
+
+
+//import com.journaldev.androidmvvmbasics.R;
+//import com.journaldev.androidmvvmbasics.databinding.ActivityMainBinding;
+//import com.journaldev.androidmvvmbasics.viewmodels.LoginViewModel;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+        activityMainBinding.executePendingBindings();
+
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
